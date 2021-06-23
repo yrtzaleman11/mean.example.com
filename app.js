9a70267c8daaed846cac254f5e9ca20af2aed7e6
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-23 15:52:56 -0500

    Implement a route for api/users

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiUsersRouter = require('./routes/api/users');
 var config = require('./config.dev');
 var mongoose = require('mongoose');
 
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
