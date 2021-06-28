f272653d9ee7c8a7526fbd850fde638ed587d843
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-28 15:24:14 -0500

    Add a non-API endpoint for accessing the user app

--- app.js
+++ app.js
@@ -4,32 +4,37 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-//~line 7 after mongoose
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+
 var Users = require('./models/users');
+
+var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
-var config = require('./config.dev');
-var apiUsersRouter = require('./routes/api/users');
 var apiAuthRouter = require('./routes/api/auth');
-//Test the file
-// console.log(config);
+var apiUsersRouter = require('./routes/api/users');
+
 var app = express();
+
+//Call the config file
+var config = require('./config.dev');
+
 //Connect to MongoDB
-mongoose.connect(config.mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//~line 32 before routes
+
 app.use(require('express-session')({
   //Define the session store
   store: new MongoStore({
@@ -47,10 +52,10 @@ app.use(require('express-session')({
     maxAge:3600000 //1 hour
   }
 }));
-passport.use(Users.createStrategy());
 app.use(passport.initialize());
 app.use(passport.session());
-//~line 53
+passport.use(Users.createStrategy());
+
 passport.serializeUser(function(user, done){
   done(null,{
     id: user._id,
@@ -60,28 +65,83 @@ passport.serializeUser(function(user, done){
     last_name: user.last_name
   });
 });
+
 passport.deserializeUser(function(user, done){
   done(null, user);
 });
-// Routes go here
+
+app.use(function(req,res,next){
+  res.locals.session = req.session;
+  next();
+});
+
+//Session based access control
+app.use(function(req,res,next){
+  //Uncomment the following line to allow access to everything.
+  //return next();
+
+  //Allow any endpoint that is an exact match. The server does not
+  //have access to the hash so /auth and /auth#xxx would bot be considered
+  //exact matches.
+  var whitelist = [
+    '/',
+    '/auth'
+  ];
+
+  //req.url holds the current URL
+  //indexOf() returns the index of the matching array element
+  //-1, in this context means not found in the array
+  //so if NOT -1 means is found in the whitelist
+  //return next(); stops execution and grants access
+  if(whitelist.indexOf(req.url) !== -1){
+    return next();
+  }
+
+  //Allow access to dynamic end points
+  var subs = [
+    '/public/',
+    '/api/auth/'
+  ];
+
+  //The query string provides a partial URL match beginning
+  //at position 0. Both /api/auth/login and /api/auth/logout would would
+  //be considered a match for /api/auth/
+  for(var sub of subs){
+    if(req.url.substring(0, sub.length)===sub){
+      return next();
+    }
+  }
+
+  //There is an active user session, allow access to all endpoints.
+  if(req.isAuthenticated()){
+    return next();
+  }
+
+  //There is no session nor are there any whitelist matches. Deny access and
+  //redirect the user to the login screen.
+  return res.redirect('/auth#login');
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/users', apiUsersRouter);
-app.use('/api/auth', apiAuthRouter);
 app.use('/auth', authRouter);
+app.use('/users', usersRouter);
+app.use('/api/auth', apiAuthRouter);
+app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-// error handler
 
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 module.exports = app;
--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -26,58 +26,56 @@ var authApp = (function() {
       app.innerHTML=form;
     }
   
-//~line 29
-function registrationForm(){
-    var app = document.getElementById('app');
-
-    var form =  `
-
-        <div class="card login-form">
-          <form id="registrationForm" class="card-body">
-            <h1 class="card-title text-center">Create an Account</h1>
-            <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
-
-            <div class="form-group">
-              <label for="first_name">First Name</label>
-              <input type="text" id="first_name" name="first_name" class="form-control" required>
-            </div>
-
-            <div class="form-group">
-              <label for="last_name">Last Name</label>
-              <input type="text" id="last_name" name="last_name" class="form-control" required>
-            </div>
-
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" id="username" name="username" class="form-control" required>
-            </div>
-
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input type="email" id="email" name="email" class="form-control" required>
-            </div>
-
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" id="password" name="password" class="form-control" required>
-            </div>
-
-            <div class="form-group">
-              <label for="confirm_password">Confirm Password</label>
-              <input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
-            </div>
-
-            <div>
-              <input type="submit" value="Sign In" class="btn btn-lg btn-primary btn-block">
-            </div>
-          </form>
-        </div>
-    `;
-
-    app.innerHTML=form;
-  }
-
-
+    function registrationForm(){
+      var app = document.getElementById('app');
+  
+      var form =  `
+  
+          <div class="card login-form">
+            <form id="registrationForm" class="card-body">
+              <h1 class="card-title text-center">Create an Account</h1>
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+  
+              <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" id="first_name" name="first_name" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" id="last_name" name="last_name" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="confirm_password">Confirm Password</label>
+                <input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
+              </div>
+  
+              <div>
+                <input type="submit" value="Sign In" class="btn btn-lg btn-primary btn-block">
+              </div>
+            </form>
+          </div>
+      `;
+  
+      app.innerHTML=form;
+    }
+  
     function postRequest(formId, url){
       let form = document.getElementById(formId);
       form.addEventListener('submit', function(e){
@@ -110,49 +108,58 @@ function registrationForm(){
       });
     }
   
-    // return {
-    //   load: function(){
-    //     loginForm();
-    //     postRequest('loginForm', '/api/auth/login');
-    //   }
-    // }
     return {
-        load: function(){
-          registrationForm();
-          postRequest('registrationForm', '/api/auth/register');
+      load: function(){
+  
+        switch(window.location.hash){
+          case '#register':
+            registrationForm();
+            postRequest('registrationForm', '/api/auth/register');
             validate.registrationForm();
-
+            break;
+  
+          default:
+            loginForm();
+            postRequest('loginForm', '/api/auth/login');
+            break;
         }
+  
       }
-    })();
-    // ~line 121
-var validate = (function() {
-
-  function confirmPasswordMatch() {
-
-    let pw = document.getElementById('password');
-    let cpw = document.getElementById('confirm_password');
-
-    if(pw.value !== cpw.value){
-      cpw.setCustomValidity("Passwords do not match");
-    } else {
-      cpw.setCustomValidity("");
     }
-
-  }
-
-  return {
-    registrationForm: function(){
-      document.querySelector('#registrationForm input[type="submit"]').addEventListener(
-        'click',
-        function(){
-        confirmPasswordMatch();
-      });
+  
+  })();
+  
+  var validate = (function() {
+  
+    function confirmPasswordMatch() {
+  
+      let pw = document.getElementById('password');
+      let cpw = document.getElementById('confirm_password');
+  
+      if(pw.value !== cpw.value){
+        cpw.setCustomValidity("Passwords do not match");
+      } else {
+        cpw.setCustomValidity("");
+      }
+  
     }
-  }
-
-})();
-
-
+  
+    return {
+      registrationForm: function(){
+        document.querySelector('#registrationForm input[type="submit"]').addEventListener(
+          'click',
+          function(){
+          confirmPasswordMatch();
+        });
+      }
+    }
+  
+  })();
   
   authApp.load();
+  
+  window.addEventListener("hashchange", function(){
+    authApp.load();
+  });
+  
+  
