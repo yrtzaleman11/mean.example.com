5c0ef786195b20c33493123ec39be7c4a5ee2d3f
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-28 16:05:49 -0500

    Add a user creation form

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -60,7 +60,58 @@ xhr.onload = function(){
   app.innerHTML = table;
 }
     }
-  
+    function createUser(){
+      var app = document.getElementById('app');
+    
+      var form =  `
+          <div class="card">
+            <div class="card-header clearfix">
+              <h2 class="h3 float-left">Create a New User</h2>
+             
+              <div class="float-right">
+                <a href="#" class="btn btn-primary">Cancel</a>
+              </div>
+            </div>
+            <div class="card-body">
+              <form id="registrationForm" class="card-body">
+                <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+    
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="first_name">First Name</label>
+                    <input type="text" id="first_name" name="first_name" class="form-control" required>
+                  </div>
+    
+                  <div class="form-group col-md-6">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" id="last_name" name="last_name" class="form-control" required>
+                  </div>
+                </div>
+    
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" required>
+                  </div>
+    
+                  <div class="form-group col-md-6">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                  </div>
+                </div>
+    
+                <div class="text-right">
+                  <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+                </div>
+              </form>
+            </div>
+          </div>
+      `;
+    
+      app.innerHTML=form;
+    }
+
+
     return {
        //~line 
 load: function(){
@@ -69,7 +120,7 @@ load: function(){
 
   switch(hashArray[0]){
     case '#create':
-      console.log('CREATE');
+      createUser();
       break;
 
     case '#view':
