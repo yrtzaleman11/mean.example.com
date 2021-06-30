e0563dfbc3ab5b3c7bb470ad2d5dd84a8ee2b26f
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-30 09:54:45 -0500

    fixing error

--- app.js
+++ app.js
@@ -1,147 +1,360 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var usersApp = (function() {
 
-var Users = require('./models/users');
+  function viewUsers(){
 
-var authRouter = require('./routes/auth');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiAuthRouter = require('./routes/api/auth');
-var apiUsersRouter = require('./routes/api/users');
+    let uri = `${window.location.origin}/api/users`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
 
-var app = express();
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
 
-//Call the config file
-var config = require('./config.dev');
+    xhr.send();
 
-//Connect to MongoDB
-mongoose.connect(config.mongodb, { useNewUrlParser: true });
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let users = data.users;
+      let table = '';
+      let rows = '';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+      //Loop each user record into it's own HTML table row, each user should
+      //have a link a user view
+      for (let i=0; i<users.length; i++) {
+        rows = rows + `<tr>
+          <td>
+            <a href="#view-${users[i]['_id']}">${users[i]['last_name']}, ${users[i]['first_name']}</a>
+          </td>
+          <td>${users[i]['username']}</td>
+          <td>${users[i]['email']}</td>
+        </tr>`;
+      }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+      //Create a users panel, add a table to the panel, inject the rows into the
+      //table
+      table = `<div class="card">
+        <div class="card-header clearfix">
+          <h2 class="h3 float-left">Users</h2>
+          <div class="float-right">
+            <a href="#create" class="btn btn-primary">New User</a>
+          </div>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-striped table-hover table-bordered">
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Username</td>
+                <td>Email</td>
+              </tr>
+            </thead>
+            <tbody>${rows}</tbody>
+          </table>
+        </div>
+      </div>`;
 
-app.use(require('express-session')({
-  //Define the session store
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection
-  }),
-  //Set the secret
-  secret: config.session.secret,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    path: '/',
-    domain: config.cookie.domain,
-    //httpOnly: true,
-    //secure: true,
-    maxAge:3600000 //1 hour
-  }
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(Users.createStrategy());
-
-passport.serializeUser(function(user, done){
-  done(null,{
-    id: user._id,
-    username: user.username,
-    email: user.email,
-    first_name: user.first_name,
-    last_name: user.last_name
-  });
-});
-
-passport.deserializeUser(function(user, done){
-  done(null, user);
-});
-
-app.use(function(req,res,next){
-  res.locals.session = req.session;
-  next();
-});
-
-//Session based access control
-app.use(function(req,res,next){
-  //Uncomment the following line to allow access to everything.
-  //return next();
-
-  //Allow any endpoint that is an exact match. The server does not
-  //have access to the hash so /auth and /auth#xxx would bot be considered
-  //exact matches.
-  var whitelist = [
-    '/',
-    '/auth'
-  ];
-
-  //req.url holds the current URL
-  //indexOf() returns the index of the matching array element
-  //-1, in this context means not found in the array
-  //so if NOT -1 means is found in the whitelist
-  //return next(); stops execution and grants access
-  if(whitelist.indexOf(req.url) !== -1){
-    return next();
-  }
-
-  //Allow access to dynamic end points
-  var subs = [
-    '/public/',
-    '/api/auth/'
-  ];
-
-  //The query string provides a partial URL match beginning
-  //at position 0. Both /api/auth/login and /api/auth/logout would would
-  //be considered a match for /api/auth/
-  for(var sub of subs){
-    if(req.url.substring(0, sub.length)===sub){
-      return next();
+      //Append the HTML to the #app
+      app.innerHTML = table;
     }
   }
 
-  //There is an active user session, allow access to all endpoints.
-  if(req.isAuthenticated()){
-    return next();
+  function createUser(){
+    var app = document.getElementById('app');
+
+    var form =  `
+        <div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">Create a New User</h2>
+            <div class="float-right">
+              <a href="#" class="btn btn-primary">Cancel</a>
+            </div>
+          </div>
+          <div class="card-body">
+            <form id="createUser" class="card-body">
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="first_name">First Name</label>
+                  <input type="text" id="first_name" name="first_name" class="form-control" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="last_name">Last Name</label>
+                  <input type="text" id="last_name" name="last_name" class="form-control" required>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username" class="form-control" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+              </div>
+
+              <div class="text-right">
+                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+              </div>
+            </form>
+          </div>
+        </div>
+    `;
+
+    app.innerHTML=form;
   }
 
-  //There is no session nor are there any whitelist matches. Deny access and
-  //redirect the user to the login screen.
-  return res.redirect('/auth#login');
+  function viewUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let card = '';
+
+      card = `<div class="card">
+        <div class="card-header clearfix">
+          <h2 class="h3 float-left">${data.user.first_name} ${data.user.last_name}</h2>
+          <div class="float-right">
+            <a href="#edit-${data.user._id}" class="btn btn-primary">Edit</a>
+          </div>
+        </div>
+        <div class="card-body">
+          <div>${data.user.username}</div>
+          <div>${data.user.email}</div>
+        </div>
+      </div>`;
+
+      app.innerHTML = card;
+    }
+  }
+
+  function editUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+
+      var form =  `
+        <div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">Edit</h2>
+            <div class="float-right">
+              <a href="#" class="btn btn-primary">Cancel</a>
+            </div>
+          </div>
+          <div class="card-body">
+            <form id="editUser" class="card-body">
+              <input type="hidden" id="_id" name="_id" value="${data.user._id}">
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="first_name">First Name</label>
+                  <input type="text" id="first_name" name="first_name" class="form-control" value="${data.user.first_name}" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="last_name">Last Name</label>
+                  <input type="text" id="last_name" name="last_name" class="form-control" value="${data.user.last_name}" required>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username" class="form-control" value="${data.user.username}" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" class="form-control" value="${data.user.email}" required>
+                </div>
+              </div>
+
+              <div class="text-right">
+                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+              </div>
+            </form>
+          </div>
+        </div>
+        <div>
+          <a href="#delete-${data.user._id}" class="text-danger">Delete</a>
+        </div>
+      `;
+
+      app.innerHTML=form;
+      processRequest('editUser', '/api/users', 'PUT');
+    }
+  }
+
+  function processRequest(formId, url, method){
+    let form = document.getElementById(formId);
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+
+      let formData = new FormData(form);
+      let uri = `${window.location.origin}${url}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open(method, uri);
+
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+
+      let object = {};
+      formData.forEach(function(value, key){
+        object[key]=value;
+      });
+
+      xhr.send(JSON.stringify(object));
+      xhr.onload = function(){
+        let data = JSON.parse(xhr.response);
+        if(data.success===true){
+          window.location.hash = `#view-${data.user._id}`
+        }else{
+          document.getElementById('formMsg').style.display='block';
+        }
+      }
+    });
+  }
+
+  function deleteView(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let card = '';
+
+      card = `<div class="card bg-transparent border-danger text-danger bg-danger">
+        <div class="card-header bg-transparent border-danger">
+          <h2 class="h3 text-center">Your About to Delete a User</h2>
+        </div>
+        <div class="card-body text-center">
+          <div>
+            Are you sure you want to delete
+            <strong>${data.user.first_name} ${data.user.last_name}</strong>
+          </div>
+
+          <div>Username: <strong>${data.user.username}</strong></div>
+          <div>Email: <strong>${data.user.email}</strong></div>
+
+          <div class="text-center">
+            <br>
+            <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+              Yes delete ${data.user.username}
+            </a>
+          </div>
+
+        </div>
+      </div>`;
+
+      app.innerHTML = card;
+    }
+  }
+
+  function deleteUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      if(data.success === true){
+        window.location.hash = '#';
+      }else{
+        alert('Unknown error, the user could not be deleted');
+      }
+
+    }
+
+  }
+
+  return {
+    load: function(){
+      // let hash = window.location.hash;
+      // let hashArray = hash.split('-');
+
+      // switch(hashArray[0]){
+        case '#create':
+          createUser();
+          processRequest('createUser', '/api/users', 'POST');
+          break;
+
+        case '#view':
+          viewUser(hashArray[1]);
+          break;
+
+        case '#edit':
+          editUser(hashArray[1]);
+          break;
+
+        case '#delete':
+          deleteView(hashArray[1]);
+          break;
+
+        default:
+          viewUsers();
+          break;
+      }
+    },
+
+    deleteUser: function(id){
+      deleteUser(id);
+    }
+
+  }
+})();
+
+usersApp.load();
+
+window.addEventListener("hashchange", function(){
+  usersApp.load();
 });
-
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/users', usersRouter);
-app.use('/api/auth', apiAuthRouter);
-app.use('/api/users', apiUsersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
